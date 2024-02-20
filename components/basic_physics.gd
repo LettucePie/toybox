0152af5b0d6ecf673e14aede89e04e2b8c4b7912
Matthew LettucePie <thelettucepie@gmail.com>
2024-02-20 09:42:16 -0500

    more notes on physics + input

--- components/basic_physics.gd
+++ components/basic_physics.gd
@@ -4,13 +4,10 @@ var translating_object : bool = false
 var translation_up : Vector3 = Vector3.UP
 var object_start_position : Vector3 = Vector3.ZERO
 var translation_start_point : Vector2 = Vector2.ZERO
+var translation_relative : Vector3 = Vector3.ZERO
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	if !self.input_event.is_connected(_on_input_event):
-		print("Automatically connecting input_event to script.")
-		self.input_event.connect(_on_input_event)
+
 
 
 func begin_translating(pressed : bool, mouse_pos : Vector2, up : Vector3):
@@ -32,10 +29,15 @@ func translate_object():
 	# I need to use on_input_event to BEGIN acknowledging input capture.
 	# then use _input(event) to dictate input to apply...
 	# But i also need to apply this in the physics_process...
-#	print("Translating object from start position: ", object_start_position)
 
 
-func _on_input_event(camera, event, position, normal, shape_idx):
+## Unhandled Input gets processed Before _input_event (physics) and after \
+## _input (main process)
+func _unhandled_input(event):
+	print("update translation_relative, then apply it in func translate_object")
+
+
+func _input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		print("Clicked! Button = ", event.button_index, " | ", event.pressed)
 		var mouse_pos : Vector2 = event.global_position
@@ -47,3 +49,8 @@ func _on_input_event(camera, event, position, normal, shape_idx):
 			begin_translating(event.pressed, 
 				mouse_pos, 
 				Vector3.FORWARD * camera.basis)
+	
+	## This won't work since it needs input to always be
+#	if event is InputEventMouseMotion and translating_object:
+#		print(event.relative)
+
