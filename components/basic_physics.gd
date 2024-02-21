84af6857d297a8dce826a3d058b512094941d5b4
Matthew LettucePie <thelettucepie@gmail.com>
2024-02-20 22:13:24 -0500

    further testing _input in relation to _physics_process

--- components/basic_physics.gd
+++ components/basic_physics.gd
@@ -1,18 +1,17 @@
 extends RigidBody3D
 
 var translating_object : bool = false
-var translation_up : Vector3 = Vector3.UP
+var floor_plane : bool = false
+var camera_dir : Vector3 = Vector3.ZERO
 var object_start_position : Vector3 = Vector3.ZERO
 var translation_start_point : Vector2 = Vector2.ZERO
-var translation_relative : Vector3 = Vector3.ZERO
+var translation_relative : Vector2 = Vector2.ZERO
 
 
-
-
-
-func begin_translating(pressed : bool, mouse_pos : Vector2, up : Vector3):
-	print("Translating off of ", up, " plane")
+func begin_translating(pressed : bool, floor : bool, mouse_pos : Vector2, cam_dir : Vector3):
 	translating_object = pressed
+	floor_plane = floor
+	camera_dir = cam_dir
 	translation_start_point = mouse_pos
 	object_start_position = self.position
 
@@ -23,34 +22,29 @@ func _physics_process(delta):
 
 
 func translate_object():
-	pass
-#	Input.
-	# Encountering issue.
-	# I need to use on_input_event to BEGIN acknowledging input capture.
-	# then use _input(event) to dictate input to apply...
-	# But i also need to apply this in the physics_process...
+	var mouse_input_dir : Vector3 = Vector3(
+		translation_relative.x,
+		translation_relative.y,
+		translation_relative.x
+	)
+	if floor_plane:
+		mouse_input_dir.z = translation_relative.y
+	translate(mouse_input_dir * camera_dir)
 
 
-## Unhandled Input gets processed Before _input_event (physics) and after \
-## _input (main process)
-func _unhandled_input(event):
-	print("update translation_relative, then apply it in func translate_object")
+func _input(event):
+	if event is InputEventMouseMotion and translating_object:
+		translation_relative = event.relative
+		print(event.relative)
+	if event is InputEventMouseButton and translating_object:
+		translating_object = event.pressed
 
 
 func _input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		print("Clicked! Button = ", event.button_index, " | ", event.pressed)
 		var mouse_pos : Vector2 = event.global_position
-		if event.button_index == 1:
-			begin_translating(event.pressed, 
-				mouse_pos, 
-				Vector3.UP)
-		elif event.button_index == 2:
-			begin_translating(event.pressed, 
-				mouse_pos, 
-				Vector3.FORWARD * camera.basis)
-	
-	## This won't work since it needs input to always be
-#	if event is InputEventMouseMotion and translating_object:
-#		print(event.relative)
-
+		begin_translating(event.pressed, 
+			event.button_index == 1,
+			mouse_pos, 
+			Vector3.UP * camera.basis)
