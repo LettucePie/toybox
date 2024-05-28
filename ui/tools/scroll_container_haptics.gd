d088de1cbcff744569bb0e662756d1c067405952
matthew <thelettucepie@gmail.com>
2024-05-27 22:21:57 -0400

    drafted ui for toyblocks

---
+++ ui/tools/scroll_container_haptics.gd
@@ -0,0 +1,19 @@
+extends ScrollContainer
+
+var clicked : bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if !is_connected("gui_input", _on_gui_input):
+		self.connect("gui_input", _on_gui_input)
+
+
+func _on_gui_input(event : InputEvent):
+	if event is InputEventMouseButton:
+		clicked = event.pressed
+	if clicked and event is InputEventMouseMotion:
+		scroll_horizontal -= event.relative.x * 1.5
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
