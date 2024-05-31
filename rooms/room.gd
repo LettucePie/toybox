6adcd8765bc7d4e794c656f2b50430629e47b6a1
matthew <thelettucepie@gmail.com>
2024-05-31 15:25:28 -0400

    camera rotate

--- rooms/room.gd
+++ rooms/room.gd
@@ -1,7 +1,13 @@
 extends Node3D
 class_name Room
 
+## Toy Management
 var toy_objects : Array = []
+
+## Camera Stuff
+@onready var cam_dolly : Node3D = $camera_doll
+var floor_clicked : bool = false
+
 
 func add_toys(objects : Array, toyname : String):
 	var new_toy : Node3D = Node3D.new()
@@ -11,3 +17,20 @@ func add_toys(objects : Array, toyname : String):
 	add_child(new_toy)
 	toy_objects.append(new_toy)
 
+#### Camera Stuff
+
+func _on_static_body_3d_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton \
+	and event.button_index == 1 \
+	and event.pressed:
+		floor_clicked = true
+
+
+func _input(event):
+	if event is InputEventMouseButton \
+	and event.button_index == 1 \
+	and !event.pressed \
+	and floor_clicked:
+		floor_clicked = false
+	if event is InputEventMouseMotion and floor_clicked:
+		cam_dolly.rotate(Vector3.UP, event.relative.x * 0.01)
