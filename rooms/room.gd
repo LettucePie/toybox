27a0cdd80d3e09ab940590fc7320b25a5b79f16d
matthew <thelettucepie@gmail.com>
2024-06-01 09:38:53 -0400

    camera zoom/angle

--- rooms/room.gd
+++ rooms/room.gd
@@ -6,6 +6,8 @@ var toy_objects : Array = []
 
 ## Camera Stuff
 @onready var cam_dolly : Node3D = $camera_doll
+@onready var cam_path : Path3D = $camera_doll/cam_path
+@onready var cam_track : PathFollow3D = $camera_doll/cam_path/cam_follow
 var floor_clicked : bool = false
 var click_index : int = 0
 
@@ -45,9 +47,11 @@ func _input(event):
 		floor_clicked = false
 	if event is InputEventMouseMotion and floor_clicked:
 		if click_index == 1:
-			print("Pan")
-			var orientated_dir = Vector3(event.relative.x, 0, event.relative.y)
+			var orientated_dir = Vector3(event.relative.x, 0, event.relative.y) * -1.0
 			cam_dolly.translate(orientated_dir * 0.01)
 		elif click_index == 2:
-			print("Rotate")
 			cam_dolly.rotate(Vector3.UP, event.relative.x * 0.01)
+			cam_track.progress_ratio = clamp(
+				cam_track.progress_ratio + event.relative.y * -0.001,
+				0.0,
+				1.0)
